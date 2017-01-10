f63de4527dfa1c920e816d3b90f948af80c20bb7
John Pham <johnphammail+tt@gmail.com>
2017-01-09 17:33:00 -0800

    email sigs

--- sms-signups/sms-signups.rb
+++ sms-signups/sms-signups.rb
@@ -2,9 +2,10 @@ require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
 
+message = "Thank you for showing interest in Citrus Hack 2017! We'll be emailing you with more information shortly. In the mean time, check out our Facebook page: https://www.facebook.com/CitrusHack/ - The Citrus Hack Team"
 get '/' do
         twiml = Twilio::TwiML::Response.new do |response|
-                response.Message "Thanks for you interest in Citrus Hack! We'll get in touch soon. \n- Citrus Hack Team"
+                response.Message message
         end
         twiml.text
 end
